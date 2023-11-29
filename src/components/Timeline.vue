34d2979b0caa3d3d1860e6560892f99cbd6e7071
alpinweb <alpinweb@yandex.ru>
2023-11-29 15:54:15 +0600

    complete store

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,67 +1,26 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import { TimelinePost, today, thisWeek, thisMonth } from "../posts.ts";
-import {DateTime} from "luxon";
 import TimelineItem from "./TimelineItem.vue";
 import {usePosts} from "../stores/posts.ts";
+import {periods} from "../constants.ts";
 
 const postsStore = usePosts();
-
-const periods = ["Today", "This Week", "This Month"] as const;
-
-type Period = typeof periods[number];
-
-const selectedPeriod = ref<Period>("Today");
-
-function selectPeriod (period: Period) {
-  selectedPeriod.value = period;
-}
-
-const posts = computed<TimelinePost[]>(() => {
-  return [
-    today,
-    thisWeek,
-    thisMonth,
-  ].map(post => {
-    return {
-      ...post,
-      created: DateTime.fromISO(post.created)
-    };
-  }).filter(post => {
-    if (selectedPeriod.value === "Today") {
-      return post.created >= DateTime.now().minus({ day: 1 });
-    }
-
-    if (selectedPeriod.value === "This Week") {
-      return post.created >= DateTime.now().minus({ week: 1 });
-    }
-
-    if (selectedPeriod.value === "This Month") {
-      return post.created >= DateTime.now().minus({ week: 4 });
-    }
-
-    return post;
-  });
-});
 </script>
 
 <template>
-  {{ postsStore.foo }}
-  <button @click="postsStore.updateFoo('bar')">Update store</button>
   <nav class="is-primary panel timeline">
     <span class="panel-tabs">
       <a
           v-for="period in periods"
           :key="period"
-          :class="{ 'is-active': period === selectedPeriod }"
-          @click="selectPeriod(period)"
+          :class="{ 'is-active': period === postsStore.selectedPeriod }"
+          @click="postsStore.setSelectedPeriod(period)"
           href="#"
       >
         {{ period }}
       </a>
     </span>
     <TimelineItem
-        v-for="post in posts"
+        v-for="post in postsStore.filteredPosts"
         :key="post.id"
         :post="post"
     >
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,17 +1,59 @@
-import { defineStore } from "pinia";
+import {defineStore} from "pinia";
+import {Post, today, thisWeek, thisMonth, TimelinePost} from "../posts.ts";
+import {Period} from "../constants.ts";
+import {computed} from "vue";
+import {DateTime} from "luxon";
 
 interface PostsState {
-    foo: string;
+    ids: string[]; // ["1", "2"]
+    all: Map<string, Post>;
+    selectedPeriod: Period;
 }
 
 export const usePosts = defineStore("posts", {
     state: (): PostsState => ({
-        foo: 'foo',
+        ids: [today.id, thisWeek.id, thisMonth.id],
+        all: new Map([
+            [today.id, today],
+            [thisWeek.id, thisWeek],
+            [thisMonth.id, thisMonth]
+        ]),
+        selectedPeriod: "Today",
     }),
 
     actions: {
-        updateFoo (foo: string) {
-            this.foo = foo;
+        setSelectedPeriod(period: Period) {
+            this.selectedPeriod = period;
+        }
+    },
+
+    getters: {
+        filteredPosts: (state): TimelinePost[] => {
+            return state.ids.map(id => {
+                const post = state.all.get(id);
+                if (!post) {
+                    throw Error(`Post with id of ${id} was not found!`);
+                }
+
+                return {
+                    ...post,
+                    created: DateTime.fromISO(post.created)
+                };
+            }).filter(post => {
+                if (state.selectedPeriod === "Today") {
+                    return post.created >= DateTime.now().minus({day: 1});
+                }
+
+                if (state.selectedPeriod === "This Week") {
+                    return post.created >= DateTime.now().minus({week: 1});
+                }
+
+                if (state.selectedPeriod === "This Month") {
+                    return post.created >= DateTime.now().minus({week: 4});
+                }
+
+                return post;
+            });
         }
     },
 });
