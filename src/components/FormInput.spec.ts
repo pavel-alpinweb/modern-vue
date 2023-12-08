037352c8329d12da5c3bcc1f2cda3f02a2d90741
alpinweb <alpinweb@yandex.ru>
2023-12-08 19:35:50 +0600

    create tests validation test

--- src/components/FormInput.spec.ts
+++ src/components/FormInput.spec.ts
@@ -1,9 +1,45 @@
 import {mount} from '@vue/test-utils';
 import {describe, it, expect} from 'vitest';
 import FormInput from "./FormInput.vue";
+import {computed, defineComponent, ref} from "vue";
 
 // .tsx
 describe("FormInput", () => {
+    it("tests validation", async () => {
+        const Parent = defineComponent({
+            components: {FormInput},
+            template: `
+              <form-input name="foo" v-model="formValue" type="input" :status="status"/>
+            `,
+            setup() {
+                const formValue = ref('foo');
+                const status = computed(() => {
+                    if (formValue.value.length > 5) {
+                        return {
+                            valid: true,
+                        };
+                    } else {
+                        return {
+                            valid: false,
+                            message: 'danger',
+                        };
+                    }
+                });
+
+                return {
+                    formValue,
+                    status,
+                };
+            },
+        });
+
+        const wrapper = mount(Parent);
+        expect(wrapper.find('.is-danger').text()).toBe('danger');
+
+        await wrapper.find('input').setValue('foobar');
+        expect(wrapper.find('.is-danger').exists()).toBe(false);
+    });
+
     it("renders some errors", () => {
         const wrapper = mount(FormInput, {
             props: {
