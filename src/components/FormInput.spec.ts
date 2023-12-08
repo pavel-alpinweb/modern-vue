eb11b4021848642313ac72ca25d67cb3ff3628a2
alpinweb <alpinweb@yandex.ru>
2023-12-08 18:11:38 +0600

    create first render tests

--- src/components/FormInput.spec.ts
+++ src/components/FormInput.spec.ts
@@ -1,8 +1,38 @@
 import {mount} from '@vue/test-utils';
-import {describe, it} from 'vitest';
+import {describe, it, expect} from 'vitest';
+import FormInput from "./FormInput.vue";
 
+// .tsx
 describe("FormInput", () => {
     it("renders some errors", () => {
-        throw Error('foo');
+        const wrapper = mount(FormInput, {
+            props: {
+                name: 'foo',
+                modelValue: 'bar',
+                status: {
+                    valid: false,
+                    message: 'error',
+                },
+                type: 'input',
+            }
+        });
+
+        expect(wrapper.find('.is-danger').exists()).toBe(true);
+    })
+
+    it("renders no errors", () => {
+        const wrapper = mount(FormInput, {
+            props: {
+                name: 'foo',
+                modelValue: 'bar',
+                status: {
+                    valid: true,
+                    message: undefined,
+                },
+                type: 'input',
+            }
+        });
+
+        expect(wrapper.find('.is-danger').exists()).toBe(false);
     })
 });
